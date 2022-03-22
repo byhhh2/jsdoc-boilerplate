7045ed9792a1c8d28aeb0fdad523ddeba7c1ccf2
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-03-22 12:17:30 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@
 
 /**
  * Buy drinks.
- * @see {@link VendingMachine} for further information.
- * @see {@link http://github.com|GitHub}
+ * @constructor
+ * @see {@link https://techcourse.woowahan.com/s/dSWvXWYI/ls/f9hI3wM3|LMS}
+ * @see {@link https://github.com/woowacourse/javascript-vendingmachine|GitHub}
  */
 function VendingMachine() {}
 
 /**
  * Buy americano depending on the price.
+ * If you enter an amount less than 1,000 won, you will get a warm Americano,
+ * and if you enter an amount more than 1,000 won, you will get a cold Americano.
  * @param {number} inputMoney Money for purchase.
- * @see {@link getItem}
- * @see getItem
+ * @example <caption>예시</caption>
+ * vendingMachine.getItem(1000);
+ * @example
+ * // === 아이스 아메리카노(1/10개)를 1000원에 구매했습니다. ===
+ * vendingMachine.getItem(900);
+ * @example
+ * // === 따뜻한 아메리카노(1/5개)를 900원에 구매했습니다. ===
+ * @see VendingMachine
  */
 VendingMachine.prototype.getItem = function (inputMoney) {};
